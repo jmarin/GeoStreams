cd6caa61858a3588c4e8021a3cc97190957bb247
jmarin <juan.marin.otero@gmail.com>
2013-07-16 18:55:17 -0400

    Add client application

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('geostreamsClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
