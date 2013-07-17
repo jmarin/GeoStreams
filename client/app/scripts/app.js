f9d8c16f37761aa194c3ce2ac83d1723982fe508
jmarin <juan.marin.otero@gmail.com>
2013-07-17 00:39:09 -0400

    Update client app

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('geostreamsClientApp', [])
+angular.module('clientApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
-angular.module('geostreamsClientApp')
+angular.module('clientApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    // $scope.awesomeThings = [
+    //   'HTML5 Boilerplate',
+    //   'AngularJS',
+    //   'Karma'
+    // ];
+
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3']
+
   });
